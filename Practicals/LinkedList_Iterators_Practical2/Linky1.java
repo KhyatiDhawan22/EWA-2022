dd0310db8cd3703b1e98b6ec8899cbb2e609996a
StressedCoder <112753105+StressedCoder@users.noreply.github.com>
2022-09-07 22:43:29 +0530

    Create Linky1.java

---
+++ Practicals/LinkedList_Iterators_Practical2/Linky1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Linky1 {
+
+	public static void main(String[] args) {
+		
+//_______________CREATING LINKEDLIST_______________
+		
+		LinkedList<Integer> Linky=new LinkedList<Integer>();
+		
+//_______________SAME FUNCTIONS AS ARRAYLIST________________
+		
+		Linky.add(1);
+		Linky.add(2);
+		Linky.remove(0);
+		Linky.size();
+		Linky.clear();
+		
+	}
+
+}
